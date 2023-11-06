f0e2fe338fb91545d8a94848bd9a6853841f4126
vivekchauhan27 <117350369+vivekchauhan27@users.noreply.github.com>
2023-11-07 01:38:43 +0530

    BT

---
+++ Crudperation.sol
@@ -0,0 +1,49 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.7;
+
+contract Crud {
+    struct User {
+        uint id;
+        string name;
+    }
+    User[] public users;
+    uint public nextid = 0;
+
+    function Create(string memory name) public {
+        users.push(User(nextid, name));
+        nextid++;
+    }
+
+    function Read(uint id) view public returns(uint, string memory) {
+        for(uint i=0; i<users.length; i++) {
+            if(users[i].id == id) {
+                return (users[i].id, users[i].name);
+            }
+        }
+        return (0, "");
+    }
+
+    function Update(uint id, string memory name) public {
+        for(uint i=0; i<users.length; i++) {
+            if(users[i].id == id) {
+                users[i].name = name;
+            }
+        }
+    }
+
+    function Delete(uint id) public {
+        delete users[id];
+    }
+
+    function find(uint id) view internal returns(uint) {
+        for(uint i=0; i<users.length; i++) {
+            if(users[i].id == id) {
+                return i;
+            }
+        }
+        // if user does not exist then revert back
+        revert("User does not exist");
+    }
+
+}
